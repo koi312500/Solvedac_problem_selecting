c96bb6c765f3089f193557b611daf47c18cb2aae
koi312500 <koi312500@gmail.com>
2023-09-21 23:08:12 +0900

    feat: select tag from input

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ url = "https://solved.ac/api/v3/search/problem"
 headers = {"Accept": "application/json"}
 
 used_problem = []
-problem_list_real = [[], [], [], [], []]
-problem_list = [[], [], [], [], []]
-query_list = ["#math", "#dp", "#dp", "#greedy", "#backtracking"]
+problem_list_real = []
+problem_list = []
+query_list = []
+tag_cnt = 0
 
-for j in range(0, 5):
+while True:
+    a = str(input("Input solvedac's tag(Enter `end` to finsh) : "))
+    if a == "end":
+        break
+    problem_list_real.append([])
+    problem_list.append([])
+    query_list.append(f"#{a}")
+    tag_cnt = tag_cnt + 1
+
+for j in range(0, tag_cnt):
     last_problem_id = 1000
     while True:
-        print(f"Debug - {last_problem_id + 1}")
         querystring = {"query":f"*b2..g %ko {query_list[j]} s#125.. id:{last_problem_id + 1}..", "sort":"id", "page":"1"}
         response = requests.get(url, headers=headers, params=querystring)
-        #print(response.json()['count'])
         if int(response.json()['count']) == 0:
             break
         try:
@@ -29,11 +37,11 @@ for j in range(0, 5):
             pass
 
     random.shuffle(problem_list[j])
+    print(f"Tag : {query_list[j]}({j+1}/{tag_cnt}) Finshed.")
 
-for i in range(0, 5):
+for i in range(0, tag_cnt):
     j = 0
-    k = 99
-    while len(problem_list_real[i]) <= k:
+    while len(problem_list_real[i]) <= 99:
         if not problem_list[i][j] in used_problem:
             problem_list_real[i].append(problem_list[i][j])
             used_problem.append(problem_list[i][j]) 
@@ -41,5 +49,5 @@ for i in range(0, 5):
 
 crawler = acmicpc_request.BOJBot("https://www.acmicpc.net/")
 crawler.login()
-for i in range(0, 5):
+for i in range(0, tag_cnt):
     crawler.self_add_problem(problem_list_real[i])
